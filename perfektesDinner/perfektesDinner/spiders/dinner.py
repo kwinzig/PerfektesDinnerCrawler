ed1f0297e6c75631b0c106ce772bece85bd59ef3
Klaus Winzig <klaus.winzig@googlemail.com>
2018-01-18 23:53:18 +0100

    Parsing List of ingredients

--- perfektesDinner/perfektesDinner/spiders/dinner.py
+++ perfektesDinner/perfektesDinner/spiders/dinner.py
@@ -12,8 +12,24 @@ class DinnerSpider(scrapy.Spider):
             yield scrapy.Request(response.urljoin(recipe_url), callback=self.parse_recipe)
         next_url_div = response.css('.voxde-pagination-list .rtli-btn-link')
         next_url = next_url_div[2].css('a::attr(href)').extract()
-        if len(next_url) > 0:
-            yield scrapy.Request(response.urljoin(next_url[0]), callback=self.parse)
+        #if len(next_url) > 0:
+        #    yield scrapy.Request(response.urljoin(next_url[0]), callback=self.parse)
 
-    def parse_recipe(sel, response):
-        pass
+    def parse_recipe(self, response):
+        recipe_name = response.css(".article-headline::text").extract_first()
+        self.get_ingredients(response)
+
+    def get_ingredients(self, response):
+        ingredients_table = response.css(".voxde-recipe-table")
+        for ingredient_row in ingredients_table[0].css("tr"):
+            if ingredient_row.css('tr::attr(rel)'):
+                ingredient_data = ingredient_row.css('td')
+                ingredient_name = ingredient_data[0].css('::text').extract_first()
+                ingredient_amount = ingredient_data[1].css('span::text').extract_first()
+                ingredient_unit = ""
+                if len(ingredient_data[1].css('::text').extract()) > 2:
+                    ingredient_unit = str.strip(ingredient_data[1].css('::text').extract()[2])
+                print(ingredient_name, ingredient_amount, ingredient_unit)
+            else:
+                meal_part = ingredient_row.css("th::text").extract_first()
+                print(meal_part)
