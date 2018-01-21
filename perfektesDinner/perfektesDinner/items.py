84ec82f2989dba5a5cded830cb98938062222fa3
Klaus Winzig <klaus.winzig@googlemail.com>
2018-01-21 22:08:50 +0100

    Renamed some variables

--- perfektesDinner/perfektesDinner/items.py
+++ perfektesDinner/perfektesDinner/items.py
@@ -17,30 +17,34 @@ class PerfektesdinnerItem(scrapy.Item):
 class RecipeItem(scrapy.Item):
     recipe_id = scrapy.Field()
     recipe_name = scrapy.Field()
-    recipe_preparation = scrapy.Field()
+    recipe_preparation_text = scrapy.Field()
     recipe_infos = scrapy.Field()
-    reicpe_ingredient_meal_parts = scrapy.Field()
+    recipe_nutrition_facts = scrapy.Field()
+    reicpe_ingredients = scrapy.Field()
 
 
-class RecipeInfos(scrapy.Item):
+class RecipeInfosItem(scrapy.Item):
     info_preparation_time = scrapy.Field()
     info_difficulty = scrapy.Field()
     info_price_range = scrapy.Field()
 
 
-class NutritionFacts(scrapy.Item):
+class NutritionFactsItem(scrapy.Item):
     kj_kcal = scrapy.Field()
     protein = scrapy.Field()
     carbohydrates = scrapy.Field()
     fat = scrapy.Field()
 
+class IngredientsItem(scrapy.Item):
+    person_quantity = scrapy.Field()
+    ingredient_meal_parts = scrapy.Field()
 
-class IngredientMealPart(scrapy.Item):
+class IngredientMealPartItem(scrapy.Item):
     mealpart_name = scrapy.Field()
     mealpart_ingredients = scrapy.Field()
 
 
-class Ingredient(scrapy.Item):
+class IngredientItem(scrapy.Item):
     ingredient_name = scrapy.Field()
     ingredient_amount = scrapy.Field()
     ingredient_unit = scrapy.Field()
